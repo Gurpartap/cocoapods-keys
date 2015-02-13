7abe6f55c23f05de2333931a849263bd9d5b89e9
Gurpartap Singh <contact@gurpartap.com>
2015-02-13 14:26:47 +0530

    Account for spaces in project name

--- lib/plugin.rb
+++ lib/plugin.rb
@@ -16,7 +16,7 @@ module CocoaPodsKeys
 
       PreInstaller.new(user_options).setup
       # Add our template podspec (needs to be remote, not local).
-      podfile.pod 'Keys', :git => 'https://github.com/ashfurrow/empty-podspec.git'
+      podfile.pod 'Keys', :git => 'https://github.com/Gurpartap/empty-podspec.git', :branch => 'project-name-space'
     end
 
     private
